aa9e5075fb800db6656d1ed85c1c09b246cb5a48
Roland Angerer <roland.angerer@beenergised.com>
2019-04-05 16:22:30 +0200

    add OCPPHandler

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 import tornado.web
+import tornado.websocket
 import tornado.ioloop
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World")
 
 
+class OCPPHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("OCPP connection established")
+
+    def on_message(self, message):
+        print("OCPP message {} received".format(message))
+    
+    def on_close(self):
+        print("OCPP connection closed")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", MainHandler)
+        (r"/", MainHandler),
+        (r"/ocpp", OCPPHandler)
     ])
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
